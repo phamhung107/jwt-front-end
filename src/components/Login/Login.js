b18093e52647665d376d4fd0730a9328b696ec62
hungpham107 <118520911+hungpham107@users.noreply.github.com>
2024-03-08 00:25:09 +0700

    Form login responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,19 @@
 import "./Login.scss";
 const Login = (props) => {
   return (
-    <div className="login-container mt-3">
+    <div className="login-container ">
       <div className="container">
-        <div className="row">
-          <div className="content-left col-7 ">
+        <div className="row px-3 px-sm-0">
+          <div className="content-left col-12 d-none col-sm-7 d-sm-block ">
             <div className="brand">Brand</div>
-            <div className="detail">Learning everything</div>
+            <div className="detail">
+              Facebook giúp bạn kết nối và chia sẻ với mọi người trong cuộc sống
+              của bạn.
+            </div>
           </div>
-          <div className="content-right col-5 green d-flex flex-column gap-3 py-3">
+
+          <div className="content-right col-sm-5 col-12  d-flex flex-column gap-3 py-3 ">
+            <div className="brand d-sm-none">Brand</div>
             <input
               type="text"
               className="form-control"
@@ -20,7 +25,11 @@ const Login = (props) => {
               placeholder="Password"
             />
             <button className="btn btn-primary">Login</button>
-            <span className="text-center">Forgot password?</span>
+            <span className="text-center">
+              <a className="forgot-password" href="#">
+                Forgot password?
+              </a>
+            </span>
             <hr />
             <div className="text-center">
               <button className="btn btn-success">Create new account</button>
