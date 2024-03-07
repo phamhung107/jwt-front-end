e4791350db0961b4245f985f88c4f1f57b92ad77
hungpham107 <118520911+hungpham107@users.noreply.github.com>
2024-03-07 17:05:11 +0700

    Form login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,34 @@
+import "./Login.scss";
 const Login = (props) => {
-  return <div className="login-container">Login</div>;
+  return (
+    <div className="login-container mt-3">
+      <div className="container">
+        <div className="row">
+          <div className="content-left col-7 ">
+            <div className="brand">Brand</div>
+            <div className="detail">Learning everything</div>
+          </div>
+          <div className="content-right col-5 green d-flex flex-column gap-3 py-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Email or phone number"
+            />
+            <input
+              type="password"
+              className="form-control"
+              placeholder="Password"
+            />
+            <button className="btn btn-primary">Login</button>
+            <span className="text-center">Forgot password?</span>
+            <hr />
+            <div className="text-center">
+              <button className="btn btn-success">Create new account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
